925fb32f98967f14a3c2ca09a703cd99108587d9
iceberg20 <italoberg@yahoo.com.br>
2019-09-13 18:00:28 -0300

    cleam project

--- server.js
+++ server.js
@@ -11,42 +11,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors()); 
 
-//Inicializando tarefas vazio
-var tarefas = new Array();
-
-var escreverArquivo = function(tarefas) {
-	fs.writeFile('tarefas.txt', JSON.stringify(tarefas), function (err) {
-  		if (err) throw err;
-	}); 
-};
-
-//Ler aquivo Json
-var readFile = function () {
-	return JSON.parse(fs.readFileSync('tarefas.txt'));
-};
-
-//Adiciona Tarefa ao arquivo Json
-var adicionaTarefas = function (req, res){
-	var task = {
-		"situacao": req.body.situacao,
-		"descricao": req.body.descricao,
-		"atribuicao": req.body.atribuicao,
-		"prazo": req.body.prazo
-	};
-	tarefas = readFile();
-	tarefas.push(task);
-	escreverArquivo(tarefas);
-	res.send(tarefas);
-};
-
 var getTarefas = function (req, res){
-	tarefas = readFile();
-	res.send(tarefas);
+  var dados = {status: "ok", app: "running"};
+
+  res.send(JSON.stringify(dados));
 };
 
 //Serviços da API
 app.get('/tarefas', getTarefas);
-app.post('/tarefas', adicionaTarefas);
 
 //Porta padrão da aplicação
 app.listen(3000, function (){
