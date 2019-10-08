3b15e83f4e010bfcbba75c4094a69b77c43586c7
iceberg20 <italoberg@yahoo.com.br>
2019-10-08 08:51:22 -0300

    add http verbs

--- server.js
+++ server.js
@@ -20,6 +20,19 @@ var getTarefas = function (req, res){
 //Serviços da API
 app.get('/tarefas', getTarefas);
 
+app.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
 //Porta padrão da aplicação
 app.listen(3000, function (){
 	console.log('Alive listening on port 3000!');
