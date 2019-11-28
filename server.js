ad79b50ad3d9db6e8182f5f3b975a753d665372c
ireis <italo.berg.residente@tre-rn.jus.br>
2019-11-28 17:52:52 -0300

    echo endpoint

--- server.js
+++ server.js
@@ -58,6 +58,40 @@ app.post('/getMovies',function (request,response)  {
   })); 
 });
 
+restService.post("/echo", function(req, res) {
+  var speech =
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.echoText
+      ? req.body.queryResult.parameters.echoText
+      : "Seems like some problem. Speak again.";
+  
+  var speechResponse = {
+    google: {
+      expectUserResponse: true,
+      richResponse: {
+        items: [
+          {
+            simpleResponse: {
+              textToSpeech: speech
+            }
+          }
+        ]
+      }
+    }
+  };
+  
+  return res.json({
+    payload: speechResponse,
+    //data: speechResponse,
+    fulfillmentText: speech,
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
+});
+
+
 
 
 
