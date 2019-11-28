329226c955fb2b032a08d23f3b8f77df729f9126
ireis <italo.berg.residente@tre-rn.jus.br>
2019-11-28 17:01:52 -0300

    1-Add unirest 2-movies endpoint

--- server.js
+++ server.js
@@ -39,6 +39,45 @@ app.delete('/', (req, res) => {
   return res.send('Received a DELETE HTTP method');
 });
 
+//Webhook
+app.get('/getName',function (req,res){
+  res.send('Swarup Bam');
+});
+
+app.post('/getMovies',function (request,response)  {
+  if(request.body.result.parameters['top-rated']) {
+      var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
+          req.query({
+              "page": "1",
+              "language": "en-US",
+              "api_key": ""
+          });
+          req.send("{}");
+          req.end(function(res) {
+              if(res.error) {
+                  response.setHeader('Content-Type', 'application/json');
+                  response.send(JSON.stringify({
+                      "speech" : "Error. Can you try it again ? ",
+                      "displayText" : "Error. Can you try it again ? "
+                  }));
+              } else if(res.body.results.length > 0) {
+                  let result = res.body.results;
+                  let output = '';
+                  for(let i = 0; i<result.length;i++) {
+                      output += result[i].title;
+                      output+="\n"
+                  }
+                  response.setHeader('Content-Type', 'application/json');
+                  response.send(JSON.stringify({
+                      "speech" : output,
+                      "displayText" : output
+                  })); 
+              }
+          });
+  }
+});
+
+
 //Porta padrão da aplicação
 app.listen(PORT, function (){
 	console.log('Second server listening on port 3000!');
