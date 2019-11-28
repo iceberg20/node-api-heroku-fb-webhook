b058f796b47d0b34d19d9d046b3cc005f694a504
ireis <italo.berg.residente@tre-rn.jus.br>
2019-11-28 17:12:20 -0300

    webhook response

--- server.js
+++ server.js
@@ -45,36 +45,11 @@ app.get('/getName',function (req,res){
 });
 
 app.post('/getMovies',function (request,response)  {
-  if(request.body.result.parameters['top-rated']) {
-      var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
-          req.query({
-              "page": "1",
-              "language": "en-US",
-              "api_key": ""
-          });
-          req.send("{}");
-          req.end(function(res) {
-              if(res.error) {
-                  response.setHeader('Content-Type', 'application/json');
-                  response.send(JSON.stringify({
-                      "speech" : "Error. Can you try it again ? ",
-                      "displayText" : "Error. Can you try it again ? "
-                  }));
-              } else if(res.body.results.length > 0) {
-                  let result = res.body.results;
-                  let output = '';
-                  for(let i = 0; i<result.length;i++) {
-                      output += result[i].title;
-                      output+="\n"
-                  }
-                  response.setHeader('Content-Type', 'application/json');
-                  response.send(JSON.stringify({
-                      "speech" : output,
-                      "displayText" : output
-                  })); 
-              }
-          });
-  }
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify({
+    "speech" : "speech do webhook",
+    "displayText" : "displayText"
+  })); 
 });
 
 
